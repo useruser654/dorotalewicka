7e08bfac5ffe454be7c5bb4fcdab05f4db7500ad
useruser654 <stazjad@o2.pl>
2026-02-04 20:44:58 +0100

    app.py

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ st.markdown("<div style='height:16px'></div>", unsafe_allow_html=True)
 
 # Opór R - suwaki podniesione bliżej wartości
 st.markdown("**Opór R [Ω]**")
-st.markdown(f"<div style='color:red; font-weight:700; margin-top:-1px; margin-bottom:-10px;'>{R:.0f} Ω</div>", unsafe_allow_html=True)
+st.markdown(f"<div style='color:red; font-weight:700; margin-top:0px; margin-bottom:-5px;'>{R:.0f} Ω</div>", unsafe_allow_html=True)
 R = st.slider("", 1.0, 500.0, R, step=1.0, key="R")
 
 # =========================

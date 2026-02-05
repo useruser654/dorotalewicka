267e3a9ec538c0ded9fcd48aec6dcfde13904aec
useruser654 <stazjad@o2.pl>
2026-02-05 03:10:46 +0100

    app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ st.slider("", 1.0, 500.0, key="R", step=0.01, on_change=update_R_from_slider)
 # =========================
 # WARTOŚCI
 # =========================
-st.markdown("<h3 style='text-align:center; margin-top:1px;'>📊 Wartości w obwodzie 📊</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align:center; margin-top:6px;'>📊 Wartości w obwodzie 📊</h3>", unsafe_allow_html=True)
 c1, c2, c3 = st.columns(3)
 c1.metric("**Natężenie I**", f"{I:.3f} A")
 c2.metric("Napięcie U", f"{U:.1f} V")
